14ae68e160d161d45ff667a9f42371d6c9a9ecf9
Chirag Aswani <chirag@aswani.net>
2019-05-10 13:34:11 -0400

    added weblinks

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tkinter.filedialog import askopenfilename
 import formatInput
 from match import Matcher
 import os
+import webbrowser
 
 
 def mentor_matching_fellows():
@@ -33,25 +34,33 @@ def generateOptimalMatches():
         f.write(str(match.pairs))
         f.close()
 
+def openMentorMatchingFellows(event):
+    webbrowser.open('https://docs.google.com/spreadsheets/d/1QViaS9TlW-5rFkk8wAgwhehgtEIgZcZVEgDX_2qQSY8/edit#gid=2111577187')
+
+def openMentorMatchingMentors(event):
+    webbrowser.open('https://docs.google.com/a/bu.edu/spreadsheets/d/1fgKFACChlMfZYBV7PNitkIa9hyb3mqYlID7xxTrmUfk/edit?usp=forms_web_b#gid=314162645')
+
 def main():
     root = Tk()
     root.title("Mentor Matching Algorithm")
 
-    row = Frame(root)
-    Label(row, width=1000, text="Mentor Matching - Students (Responses)", anchor='w').pack(side=LEFT)
-    row.pack(side=TOP, fill=X)
 
     row = Frame(root)
-    Label(row, width=1000, text="https://docs.google.com/spreadsheets/d/1QViaS9TlW-5rFkk8wAgwhehgtEIgZcZVEgDX_2qQSY8/edit#gid=2111577187", anchor='w').pack(side=LEFT)
+    Label(row, width=1000, text="", anchor='w').pack(side=LEFT)
     row.pack(side=TOP, fill=X, padx=5, pady=5)
 
     row = Frame(root)
-    Label(row, width=1000, text="Mentor Matching - Mentor (Responses)", anchor='w').pack(side=LEFT)
-    row.pack(side=TOP, fill=X)
-
-    row = Frame(root)
-    Label(row, width=1000, text="https://docs.google.com/a/bu.edu/spreadsheets/d/1fgKFACChlMfZYBV7PNitkIa9hyb3mqYlID7xxTrmUfk/edit?usp=forms_web_b#gid=314162645", anchor='w').pack(side=LEFT)
+    Label(row, width=1000, text="", anchor='w').pack(side=LEFT)
     row.pack(side=TOP, fill=X, padx=5, pady=5)
+
+    hyper_text = Label(root, text="Mentor Matching - Students (Responses)", fg="blue", cursor="hand2")
+    hyper_text.pack(side=LEFT, padx=5, pady=5)
+    hyper_text.bind("<Button-1>", openMentorMatchingFellows)
+
+    hyper_text = Label(root, text="Mentor Matching - Mentor (Responses)", fg="blue", cursor="hand2")
+    hyper_text.pack(side=LEFT, padx=5, pady=5)
+    hyper_text.bind("<Button-1>", openMentorMatchingMentors)
+
     buttonTTK = ttk.Button(root, text='Import Fellowship Preferences', command=(lambda: mentor_matching_fellows() )).pack(side=LEFT)
     buttonTTK2 = ttk.Button(root, text='Import Mentor Preferences', command=(lambda: mentor_matching_mentors() )).pack(side=LEFT)
     buttonTTK3 = ttk.Button(root, text='Generate Optimal Matches', command=(lambda: generateOptimalMatches() )).pack(side=LEFT)
